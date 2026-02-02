9d85dc6f03e74486caf200bee2bb59fd9d571e79
Yonathan <yonathan.hunetaw@gmail.com>
2026-02-02 06:55:32 +0300

    add vite react plugin and change file name from '.js' to '.jsx' then create the Pages folder and change the route

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -3,7 +3,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    @vite('resources/js/app.js')
+    @viteReactRefresh
+    @vite('resources/js/app.jsx')
     @inertiaHead
 </head>
 <body>
