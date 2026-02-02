21c11e32d35f9bee34b1055f249f349b53fa6b79
yonathan hunetaw <160973598+wibdar@users.noreply.github.com>
2026-02-02 07:17:15 +0300

    Alias for resources/js and tailwind check to see if it works

--- resources/js/Pages/Home.jsx
+++ resources/js/Pages/Home.jsx
@@ -1,7 +1,7 @@
 export default function Home() {
     return (
-        <div>
-            <h1>Duka</h1>
-        </div>
+        <>
+            <h1 className="title">Duka</h1>
+        </>
     )
 }   
