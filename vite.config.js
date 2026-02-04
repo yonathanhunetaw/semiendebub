36ba378512e860646e142b3d5fda76c1c7243d7f
Yonathan <yonathan.hunetaw@gmail.com>
2026-02-04 03:58:56 +0300

    very important as when runing docker exec -it Duka npm run dev vite works differently and so we need to add the server section so vite can send over its local docker dev compiled staff to local machine

--- vite.config.js
+++ vite.config.js
@@ -1,25 +1,22 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
-import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: 'resources/js/app.jsx', 
+            input: 'resources/js/app.jsx',
             refresh: true,
         }),
-        tailwindcss(),
         react(),
     ],
-    resolve:{
-        alias: {
-            "@": "/resources/js",
-        },
-    },
+    // ADD THIS SECTION BELOW:
     server: {
-        watch: {
-            ignored: ['**/storage/framework/views/**'],
+        host: '0.0.0.0', // Necessary for Docker to expose the service
+        port: 5173,
+        strictPort: true,
+        hmr: {
+            host: 'localhost', // Tells your browser to look at localhost, not [::1]
         },
     },
 });
