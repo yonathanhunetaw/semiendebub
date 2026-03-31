474ba04f3c195137528422eb0d48f43243e5cb0d
Yonathan <yonathan.hunetaw@gmail.com>
2026-03-31 04:25:08 +0300

    proxie truest

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -25,6 +25,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
+        $middleware->trustProxies(at: '*');
         $middleware->alias([
             'role' => RoleMiddleware::class,
             'role.subdomain' => EnsureCorrectSubdomainRole::class,
