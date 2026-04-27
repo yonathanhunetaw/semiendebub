87780779c21c6e01aa0b07957f3aabf4d9930044
Yonathan <103480325+yonathanhunetaw@users.noreply.github.com>
2026-04-27 06:19:23 +0300

    t

--- resources/js/Layouts/AdminLayout.tsx
+++ resources/js/Layouts/AdminLayout.tsx
@@ -65,12 +65,12 @@ export default function AdminLayout({ children }: Props) {
                     href={`data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22>
                         <rect width=%22100%22 height=%22100%22 rx=%2220%22 fill=%22${encodeURIComponent(theme.palette.primary.main)}%22/>
                         <text
-                            y=%2235%22
-                            x=%2215%22
-                            font-size=%2230%22
+                            y=%2255%22
+                            x=%2210%22
+                            font-size=%2255%22
                             fill=%22white%22
                             font-family=%22sans-serif%22
-                            font-weight=%22bold%22
+                            font-weight=%22900%22
                             text-anchor=%22start%22
                         >
                             ${config.label.charAt(0)}
