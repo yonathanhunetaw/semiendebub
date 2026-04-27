db5faba4c9d27df844d40baa2cab66a08ae66a60
Yonathan <103480325+yonathanhunetaw@users.noreply.github.com>
2026-04-27 05:57:39 +0300

    e

--- resources/js/Layouts/GuestLayout.jsx
+++ resources/js/Layouts/GuestLayout.jsx
@@ -1,16 +1,16 @@
-import ApplicationLogo from '@/Components/ApplicationLogo';
+import ApplicationLogo from "@/Components/Navigation/Global/ApplicationLogo";
 import { Link } from '@inertiajs/react';
 
 export default function GuestLayout({ children }) {
     return (
-        <div className="flex min-h-screen flex-col items-center bg-gray-100 pt-6 sm:justify-center sm:pt-0">
+        <div className="flex flex-col items-center min-h-screen pt-6 bg-gray-100 sm:justify-center sm:pt-0">
             <div>
                 <Link href="/">
-                    <ApplicationLogo className="h-20 w-20 fill-current text-gray-500" />
+                    <ApplicationLogo className="w-20 h-20 text-gray-500 fill-current" />
                 </Link>
             </div>
 
-            <div className="mt-6 w-full overflow-hidden bg-white px-6 py-4 shadow-md sm:max-w-md sm:rounded-lg">
+            <div className="w-full px-6 py-4 mt-6 overflow-hidden bg-white shadow-md sm:max-w-md sm:rounded-lg">
                 {children}
             </div>
         </div>
