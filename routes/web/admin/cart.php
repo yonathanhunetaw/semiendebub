f79c944ec41615b5c2e89cae84297d2031693d2e
Yonathan <103480325+yonathanhunetaw@users.noreply.github.com>
2026-04-30 09:40:42 +0300

    bug

--- routes/web/admin/cart.php
+++ routes/web/admin/cart.php
@@ -7,12 +7,14 @@ $baseDomain = config('app.system_domain', 'duka.local');
 
 Route::domain("admin.{$baseDomain}")
     ->middleware(['auth', 'verified', 'role.subdomain:admin'])
-    ->prefix('carts')
+    ->prefix('carts') // Everything inside this group starts with /carts
     ->group(function () {
         Route::get('/', [CartController::class, 'index'])->name('admin.carts.index');
+
+        // FIX: Just use '/' or 'create' because 'carts' is already prefixed
+        Route::get('/create', [CartController::class, 'create'])->name('admin.carts.create');
+        Route::post('/', [CartController::class, 'store'])->name('admin.carts.store');
+
         Route::get('/{id}', [CartController::class, 'show'])->name('admin.carts.show');
         Route::delete('/{id}', [CartController::class, 'destroy'])->name('admin.carts.destroy');
-
-        // If you are using the 'store' method we discussed earlier:
-        Route::post('/', [CartController::class, 'store'])->name('admin.carts.store');
     });
