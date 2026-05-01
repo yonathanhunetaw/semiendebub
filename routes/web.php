47af03c14eb56dda7455fde89436aed53a2e425f
Yonathan <103480325+yonathanhunetaw@users.noreply.github.com>
2026-05-01 10:16:57 +0300

    .php

--- routes/web.php
+++ routes/web.php
@@ -30,9 +30,9 @@ require __DIR__.'/auth.php';                                // ['auth', 'verifie
 
 require __DIR__.'/web/admin/admin.php';                     // ['auth', 'verified', 'check_role:Admin']
 require __DIR__.'/web/admin/cart.php';
-require __DIR__.'/web/admin/inventory';
+require __DIR__.'/web/admin/inventory.php';
 
-require __DIR__.'/web/admin/store';
+require __DIR__.'/web/admin/store.php';
 
 
 /*-------------------------------------------------------------------------------------------------------------
