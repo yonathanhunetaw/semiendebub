a253811bc6e8483bb01bb74e25de5b26f1aa663d
Yonathan <103480325+yonathanhunetaw@users.noreply.github.com>
2026-05-09 08:08:57 +0300

    warehouse to be associated with a store

--- database/migrations/2026_05_09_040815_create_warehouses_table.php
+++ database/migrations/2026_05_09_040815_create_warehouses_table.php
@@ -12,6 +12,7 @@ return new class extends Migration {
             $table->string('name');
             $table->string('code')->unique()->nullable(); // e.g., WH-MAIN-01
             $table->string('address')->nullable();
+            $table->foreignId('store_id')->nullable()->constrained()->nullOnDelete();
             $table->string('manager')->nullable();
             $table->enum('status', ['active', 'inactive'])->default('active');
             $table->timestamps();
