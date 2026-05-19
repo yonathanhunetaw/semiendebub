3df33f763b491b38674f2b9a8a12042d30f5f22c
Yonathan <103480325+yonathanhunetaw@users.noreply.github.com>
2026-05-19 18:10:21 +0300

    signature_version' => 'v4',

--- config/filesystems.php
+++ config/filesystems.php
@@ -58,6 +58,7 @@ return [
             'use_path_style_endpoint' => env('AWS_USE_PATH_STYLE_ENDPOINT', false),
             'throw' => true,   // <--- CHANGE THIS FROM false TO true
             'report' => false,
+            'signature_version' => 'v4',
         ],
 
     ],
