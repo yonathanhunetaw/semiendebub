acc17dd7bcd40d7db0e62a925023ac9cfc692721
Yonathan <103480325+yonathanhunetaw@users.noreply.github.com>
2026-06-06 04:59:23 +0300

    imports

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,7 @@
+import { defineConfig, loadEnv } from 'vite';
+import laravel from 'laravel-vite-plugin';
+import react from '@vitejs/plugin-react';
+
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, process.cwd(), '');
     const appDomain = env.APP_DOMAIN || 'duka2.pi';
