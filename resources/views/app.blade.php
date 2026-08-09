83d8d95ca967a11848cd41874176c81eace96baf
Yonathan <103480325+yonathanhunetaw@users.noreply.github.com>
2026-08-09 05:05:21 +0300

    analetics

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,24 +1,40 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title inertia>{{ config('app.name', 'Welcome to') }}</title>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+    <title inertia>{{ config('app.name', 'Welcome to') }}</title>
 
-        <!-- Scripts -->
-        @routes
-        @if (file_exists(public_path('hot')))
-            @viteReactRefresh
-        @endif
-        @vite(['resources/js/app.tsx'])
-        @inertiaHead
-    </head>
-    <body class="font-sans antialiased">
-        @inertia
-    </body>
+    @if (app()->environment('production') && config('services.google.analytics_id'))
+        <!-- Google Analytics -->
+        <script async
+            src="https://www.googletagmanager.com/gtag/js?id={{ config('services.google.analytics_id') }}"></script>
+        <script>
+            window.dataLayer = window.dataLayer || [];
+            function gtag() { dataLayer.push(arguments); }
+            gtag('js', new Date());
+
+            gtag('config', '{{ config('services.google.analytics_id') }}');
+        </script>
+    @endif
+
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+
+    <!-- Scripts -->
+    @routes
+    @if (file_exists(public_path('hot')))
+        @viteReactRefresh
+    @endif
+    @vite(['resources/js/app.tsx'])
+    @inertiaHead
+</head>
+
+<body class="font-sans antialiased">
+    @inertia
+</body>
+
 </html>
