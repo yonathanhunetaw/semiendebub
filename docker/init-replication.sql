576a452a06e3ae27830e2df7713e78a49e3763c0
Yonathan <103480325+yonathanhunetaw@users.noreply.github.com>
2026-08-30 04:18:20 +0300

    ALTER USER 'replicator'@'%' IDENTIFIED WITH mysql_native_password BY 'secure_replication_password';

--- docker/init-replication.sql
+++ docker/init-replication.sql
@@ -1,3 +1,4 @@
 CREATE USER IF NOT EXISTS 'replicator'@'%' IDENTIFIED BY 'secure_replication_password';
+ALTER USER 'replicator'@'%' IDENTIFIED WITH mysql_native_password BY 'secure_replication_password';
 GRANT REPLICATION SLAVE ON *.* TO 'replicator'@'%';
 FLUSH PRIVILEGES;
